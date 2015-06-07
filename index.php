7d8ed4c962d150301c197008c76c757c902c6220
Jesse Carter <jctr073@gmail.com>
2015-06-06 20:41:17 -0700

    Adding another message

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 	<title>Action Recreational Park</title>
 </head>
 <body>
-	<?php echo "The ARP site is under construction. Come back soon!" ?>
+	<?php echo "The ARP site is under construction. Come back soon!"; ?>
+	<br>
+	<?php echo "more info"; ?>
 </body>
 </html>
